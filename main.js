6e89b0f0f8a4d303427d75739053d67379b60f04
Johnodon <jbost0114@gmail.com>
2021-02-20 22:18:14 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,70 @@
+require('dotenv').config();
+const {app, BrowserWindow, ipcMain, dialog} = require('electron')
+const path = require('path');
+const fs = require('fs');
+
+app.commandLine.appendSwitch('autoplay-policy', 'no-user-gesture-required');
+app.commandLine.appendSwitch('disable-web-security');
+global.appdatapath = app.getPath('appData') + '\\audiofy';
+if(!fs.existsSync(global.appdatapath + '/config.json')) fs.writeFileSync(global.appdatapath + '/config.json', JSON.stringify({"dir":""}), {encoding: 'utf-8'});
+
+
+
+
+let loading;
+let settings;
+let main;
+app.on('ready', ()=>{
+  loading = new BrowserWindow({width: 375, height: 500, frame: false, webPreferences: {nodeIntegration:true,enableRemoteModule:true}});
+  settings = new BrowserWindow({width: 375, height: 500, frame: false, webPreferences: {nodeIntegration:true,enableRemoteModule:true}, show: false});
+
+  loading.loadFile('loading.html');
+  settings.loadFile('settings.html');
+  
+  main = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    frame: false,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      enableRemoteModule: true,
+      webSecurity: false,
+    },
+    show: false,
+  });
+  main.loadFile('index.html')
+  main.webContents.once('dom-ready', () => {
+    setTimeout(()=>{
+      main.show();
+      loading.hide();
+    }, 3000);
+  });
+  
+});
+ipcMain.on('songSize', (e, n)=>{
+  loading.webContents.send('songSize', n);
+});
+ipcMain.on('songAdded', (e, n)=>{
+  loading.webContents.send('songAdded', 1);
+});
+ipcMain.on('close-app', (e)=>{
+  app.quit();
+});
+ipcMain.on('openSettings', (e)=>{
+  if(settings.isVisible()) return;
+  else settings.show();
+});
+ipcMain.on('refreshMain', (e)=>{
+  return main.reload();
+});
+ipcMain.on('openDir', async (e)=>{
+  var path = await dialog.showOpenDialog({
+    properties: ['openDirectory']
+});
+settings.webContents.send('dialogFinished', path);
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
---
+++ settingsrenderer.js
@@ -0,0 +1,28 @@
+const {remote, ipcRenderer} = require('electron');
+const fs = require('fs');
+
+
+global.appdatapath = remote.app.getPath('appData') + '\\audiofy';
+let config = JSON.parse(fs.readFileSync(global.appdatapath + '/config.json', {encoding: 'utf-8'}));
+document.addEventListener('DOMContentLoaded', function(e){
+    document.getElementById('close-button').addEventListener('click', function(e){
+        remote.getCurrentWindow().hide();
+    });
+    document.getElementById('inputDir').value = config.dir;
+    document.getElementById('openDir').addEventListener('click', function(e){
+        ipcRenderer.send('openDir');
+    });
+    ipcRenderer.on('dialogFinished', function(e, dialog){
+        if(dialog.canceled == false) document.getElementById('inputDir').value = dialog.filePaths[0];
+        else return;
+    });
+    document.getElementById('settingsSave').addEventListener('click', function(e){
+        let final = {
+            "dir": document.getElementById('inputDir').value
+        }
+        fs.writeFileSync(global.appdatapath + '/config.json', JSON.stringify(final), {encoding: 'utf-8'});
+        ipcRenderer.send('refreshMain');
+        remote.getCurrentWindow().hide();
+
+    });
+});
